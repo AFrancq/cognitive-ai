037c9cd00fef3a74b478b94f8473822dad67717b
Ash Francq <ash.francq@gmail.com>
2026-01-09 23:35:56 -0600

    Bodypart headerfile

--- include/bodypart.h
+++ include/bodypart.h
@@ -2,23 +2,46 @@
 Bodypart class header file
 
 Virtual class representing a body part.
+Each body part has a reference to the brain for sending sensory data.
 */
 
 #ifndef BODYPART_H
 #define BODYPART_H
 
-#include <vector>
+#include <string>
+#include <memory>
+
+// Forward declaration
+class Brain;
 
 class Bodypart {
     public:
         Bodypart();
-        virtual ~Bodypart();
+        virtual ~Bodypart() = default;
 
-        virtual void info() = 0;
-        virtual void initialize() = 0;
+        // Display information about the body part
+        virtual void info() const = 0;
+
+        // Initialize the body part
+        virtual bool initialize() = 0;
+
+        // Shutdown the body part
         virtual void shutdown() = 0;
-    private:
+
+        // Check if the body part is active
+        virtual bool isActive() const { return active; }
+
+        // Set the brain reference for sending data
+        void setBrain(std::shared_ptr<Brain> brain);
+
+    protected:
+        std::shared_ptr<Brain> brain;
+        bool active = false;
         unsigned int id;
+        std::string name;
+
+    private:
+        static unsigned int nextId;
 };
 
 #endif // BODYPART_H
